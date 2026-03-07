9f5100360fc4b93d2007443f99a663b203a14435
Alvaroarnanz <alvaroarnanzlemaire@gmail.com>
2026-03-07 22:02:29 +0100

    fix

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,12 +1,6 @@
 import "dotenv/config"
 import { Pool } from "pg"
 
-console.log("DB_HOST:", process.env.DB_HOST)
-console.log("DB_USER:", process.env.DB_USER)
-console.log("DB_PASSWORD existe:", !!process.env.DB_PASSWORD)
-console.log("DB_NAME:", process.env.DB_NAME)
-console.log("DB_PORT:", process.env.DB_PORT)
-
 const host = process.env.DB_HOST
 const user = process.env.DB_USER
 const password = process.env.DB_PASSWORD
