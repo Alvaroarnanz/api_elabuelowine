67bdb962a54f1a931194a1ff6948cbc6cfdf3972
Alvaroarnanz <alvaroarnanzlemaire@gmail.com>
2026-03-07 21:40:15 +0100

    try to fix

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,12 @@ import { pool } from "./config/db"
 const app = express()
 const PORT = process.env.PORT || 3001
 
+console.log("DB_HOST:", process.env.DB_HOST)
+console.log("DB_USER:", process.env.DB_USER)
+console.log("DB_PASSWORD existe:", !!process.env.DB_PASSWORD)
+console.log("DB_NAME:", process.env.DB_NAME)
+console.log("DB_PORT:", process.env.DB_PORT)
+
 const allowedOrigins = [
   "http://localhost:5173",
   "https://app.elabuelowine.com",
